4b60f7c53f25ece04cde362ec0a45cd3e346c222
Florent C. L. M. Michel <56166507+FlorentCLMichel@users.noreply.github.com>
2022-01-22 12:05:38 +0000

    share the database between tasks

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use tokio::net::{ TcpListener, TcpStream };
 use mini_redis::{ Connection, Frame, Result };
 use mini_redis::Command::{ self, Get, Set };
 use std::collections::HashMap;
+use std::sync::{ Arc, Mutex };
+use bytes::Bytes;
+
+// type for a database which can be shared between threads
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 const ADDRESS: &str = "127.0.0.1";
 const PORT: &str = "6379";
@@ -13,26 +18,29 @@ async fn main() {
     // create a listener bound the listener to the address
     let listener = TcpListener::bind(&format!("{}:{}", ADDRESS, PORT)).await.unwrap();
 
+    // initialize the database
+    let db: Db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         
         // get the socket of the new connection
         let (socket, _) = listener.accept().await.unwrap();
 
+        // clone the handle to the database
+        let db = db.clone();
+
         // spawn a task to process the socket
         tokio::spawn(async move {
-            process(socket).await.unwrap();
+            process(socket, db).await.unwrap();
         });
     }
 }
 
 
-async fn process(socket: TcpStream) -> Result<()> {
+async fn process(socket: TcpStream, db: Db) -> Result<()> {
 
     // read/write as redis ‘frames’ instead of a byte stream
     let mut connection = Connection::new(socket);
-
-    // hashmap used to store data
-    let mut db = HashMap::new();
 
     while let Some(frame) = connection.read_frame().await? {
 
@@ -40,11 +48,19 @@ async fn process(socket: TcpStream) -> Result<()> {
             
             // store the value as a `Vec<u8>`
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                
+                // lock the database
+                let mut db = db.lock().unwrap();
+
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             },
 
             Get(cmd) => {
+                
+                // lock the database
+                let db = db.lock().unwrap();
+                
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
