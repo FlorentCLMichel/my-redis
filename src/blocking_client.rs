a123f2afb25663725fd303c8ffcc1e54f24d7a7d
Florent C. L. M. Michel <56166507+FlorentCLMichel@users.noreply.github.com>
2022-01-29 00:19:02 +0000

    complete the synchronous client example

--- src/blocking_client.rs
+++ src/blocking_client.rs
@@ -2,6 +2,8 @@
 
 use tokio::net::ToSocketAddrs;
 use tokio::runtime::Runtime;
+use bytes::Bytes;
+use std::time::Duration;
 
 pub use crate::client::Message;
 
@@ -15,10 +17,71 @@ pub struct BlockingClient {
     rt: Runtime,
 }
 
-pub fn conect<T: ToCocketAddrs>(addr: T) -> crate::Result<BlockingClient> {
+/// a client that has entered pub/sub mode
+pub struct BlockingSubscriber {
+    
+    // the asynchronous subscriber
+    inner: crate::client::Subscriber,
+
+    // a `current_thread` runtime for executing operations on the client in a blocking manner
+    rt: Runtime,
+}
+
+pub fn connect<T: ToCocketAddrs>(addr: T) -> crate::Result<BlockingClient> {
     let rt = tokio::runtime::Builder::new_current_thread()
               .enable_all()
               .build()?;
     let inner = rt.block_on(crate::client::connect(addr))?;
     Ok(BlockingClient { inner, rt })
 }
+
+impl BlockingClient {
+
+    pub fn get(&mut self, key: &str) -> crate::Result<Option<Bytes>> {
+        self.rt.block_on(self.inner.get(key))
+    }
+
+    pub fn get(&mut self, key: &str, vaue: Bytes) -> crate::Result<()> {
+        self.rt.block_on(self.inner.set(key, value))
+    }
+
+    pub fn set_expires(&mut self, key: &str, value: Bytes, expiration: Duration) 
+        -> crate::Result<()> 
+    {
+        self.rt.block_on(self.inner.set_expires(key, value, expiration))
+    }
+
+    pub fn publish(&mut self, channel: &str, message: Bytes) -> crate::Result<u64>
+    {
+        self.rt.block_on(self.inner.publish(channel, message))
+    }
+
+    pub fn subscribe(self, channels: Vec<String>) -> crate::Result<BlockingSubscriber>
+    {
+        let subscriber = self.rt.block_on(self.inner.subscribe(channels))?;
+        Ok(BlockingSubscriber { 
+            inner: subscriber,
+            rt: self.rt
+        })
+    }
+}
+
+impl BlockingSubscriber {
+
+    pub fn get_subscribed(&self) -> &{String] {
+        self.inner.get_suscribed()
+    }
+    
+    pub fn next_message(&mut self) -> crate::Result<Option<Message>> {
+        self.rt.block_on(self.inner.next_message())
+    }
+    
+    pub fn subscribe(&mut self, channels: &[String]) -> crate::Result<()> {
+        self.rt.block_on(self.inner.subscriber(channels))
+    }
+    
+    pub fn unsubscribe(&mut self, channels: &[String]) -> crate::Result<()> {
+        self.rt.block_on(self.inner.unsubscriber(channels))
+    }
+}
+
